ae277fbdaa44b61df2d2c0d76faf96a1046cc29c
andreimironenko <andrei.mironenko@gmail.com>
2023-07-13 18:20:42 +0100

    Move to docker volumes instead of sharing host file system directories

--- makeenv.sh
+++ makeenv.sh
@@ -8,14 +8,8 @@ uid="$(id -u)"
 gid="$(id -g)"
 username="$(id -u -n)"
 
-pushd .. &> /dev/null
-workspace="$(pwd)"
-popd &> /dev/null
-
-
 echo "uid=${uid}" >.env
 echo "gid=${gid}" >>.env
 echo "username=${username}" >>.env
-echo "workspace=${workspace}" >>.env
 
 echo "Success: Docker environment file .env is created"
