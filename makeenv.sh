4050d2e1e5d24eaeeb456cf75a54ffd42f95482f
Andrei Mironenko <andrei.mironenko@gmail.com>
2025-06-19 11:18:25 +0000

    Creating multi-user shared container. All local users are also recreated in the container with the same username/groupname and uid/gid as in the host system

--- makeenv.sh
+++ makeenv.sh
@@ -4,12 +4,32 @@
 # will consist of current user UID and GID, username,
 # and some other variables
 
-uid="$(id -u)"
-gid="$(id -g)"
-username="$(id -u -n)"
+#uid="$(id -u)"
+#gid="$(id -g)"
+#username="$(id -u -n)"
+#
+#echo "uid=${uid}" >.env
+#echo "gid=${gid}" >>.env
+#echo "username=${username}" >>.env
 
-echo "uid=${uid}" >.env
-echo "gid=${gid}" >>.env
-echo "username=${username}" >>.env
+user_list=""
+
+for d in /home/*; do
+  uname=$(basename "$d")
+  uid=$(id -u "$uname" 2>/dev/null)
+  gid=$(id -g "$uname" 2>/dev/null)
+  gname=$(id -gn "$uname" 2>/dev/null)
+  # Remove any spaces in the gname
+  gname="${gname// /}" 
+  if [ "$uid" -ge 1000 ] 2>/dev/null; then
+    user_list+="$uname:$gname:$uid:$gid,"
+  fi
+
+done
+
+docker_gid="$(getent group docker | cut -d: -f3 2>/dev/null)"
+
+echo "user_list=\"${user_list}\"" >.env
+echo "docker_gid=${docker_gid}" >>.env
 
 echo "Success: Docker environment file .env is created"
