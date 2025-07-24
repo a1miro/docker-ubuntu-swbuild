3093e01163561e82eed20cf335a33cbe91c93598
Andrei Mironenko <andrei.mironenko@paralleldynamic.com>
2025-07-24 08:07:21 +0100

    Updating makeenv.sh with generation of Docker entry point shell script, which generates new users home directories at the start up of the container

--- makeenv.sh
+++ makeenv.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-# This script creates .env file for docker-compose and
-# will consist of current user UID and GID, username,
-# and some other variables
+# This shell script creates .env environment which contains a list of the host users, 
+# adding the username, usergroup, UID and GID in the user_list variable. 
+# The host docker group GID is also added to the .env file. The .env file is 
+# automatically sourced by docker compose during container build.
 
-#uid="$(id -u)"
-#gid="$(id -g)"
-#username="$(id -u -n)"
-#
-#echo "uid=${uid}" >.env
-#echo "gid=${gid}" >>.env
-#echo "username=${username}" >>.env
+# Another file docker_entrypoint.sh is created. This is the entrypoint for the docker container.
+# It checks if the home directory for each user exists in the current home directories volume, 
+# and if not, it creates the home directory and copies the contents of /etc/skel. The generated script 
+# is copied to the container and executed as the Docker entrypoint script.
 
+
+# Creating user_list from existing users in /home folder of the host system
 user_list=""
-
 for d in /home/*; do
   uname=$(basename "$d")
   uid=$(id -u "$uname" 2>/dev/null)
@@ -27,9 +26,29 @@ for d in /home/*; do
 
 done
 
+# Retrive the docker group GID
 docker_gid="$(getent group docker | cut -d: -f3 2>/dev/null)"
 
+# Create .env file
 echo "user_list=\"${user_list}\"" >.env
 echo "docker_gid=${docker_gid}" >>.env
 
-echo "Success: Docker environment file .env is created"
+# Create docker_entrypoint.sh script
+echo "user_list=\"${user_list}\"" > docker_entrypoint.sh
+echo "IFS=','" >> docker_entrypoint.sh
+echo 'for entry in ${user_list}; do' >> docker_entrypoint.sh
+echo "    uname=\$(echo \$entry | cut -d: -f1)" >> docker_entrypoint.sh
+echo "    uid=\$(echo \$entry | cut -d: -f3)" >> docker_entrypoint.sh
+echo "    home_dir=\$(getent passwd \"\$uname\" | cut -d: -f6)" >> docker_entrypoint.sh
+echo "    if [ -n \"\$home_dir\" ] && [ ! -d \"\$home_dir\" ]; then" >> docker_entrypoint.sh
+echo "        mkdir -p \"\$home_dir\"" >> docker_entrypoint.sh
+echo "        cp -rT /etc/skel \"\$home_dir\"" >> docker_entrypoint.sh
+echo "        chown -R \"\$uname\":\"\$uname\" \"\$home_dir\"" >> docker_entrypoint.sh
+echo "        echo \"Created home for \$uname at \$home_dir\"" >> docker_entrypoint.sh
+echo "    fi" >> docker_entrypoint.sh
+echo "done" >> docker_entrypoint.sh
+
+# Make the script executable
+chmod +x docker_entrypoint.sh
+
+echo "Success: Docker environment file .env and docker_entrypoint.sh are created"
